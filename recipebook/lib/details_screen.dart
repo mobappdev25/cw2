2272860eb294cda7656b1e9a71015847eaa2aeed
Emma <emmab0118@gmail.com>
2025-10-05 20:30:14 -0400

    added some images

--- recipebook/lib/details_screen.dart
+++ recipebook/lib/details_screen.dart
@@ -9,24 +9,41 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(recipe['name']!)),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Display large image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                recipe['image']!,
+                width: double.infinity,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 20),
             Text(
               'Ingredients:',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             Text(recipe['ingredients']!, style: TextStyle(fontSize: 16)),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Text(
               'Instructions:',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             Text(recipe['instructions']!, style: TextStyle(fontSize: 16)),
+            const SizedBox(height: 20),
+            Text(
+              'From:',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
+            Text(recipe['from']!, style: TextStyle(fontSize: 10)),
           ],
         ),
       ),
--- recipebook/lib/home_screen.dart
+++ recipebook/lib/home_screen.dart
@@ -6,18 +6,21 @@ class HomeScreen extends StatelessWidget {
     {'name': "Chicken Piccata",
       'ingredients': '2 Chicken Breasts,  6 Tbsp Butter, 5 Tbsp Olive Oil, ~1/3 Cup Lemon Juice, Flour, 1/2 Cup Chicken Stock, 1/3 Cup Capers, 1/3 Cup Parsley',
       'instructions': 'Season chicken breasts and dredge in flour. Melt butter in skillet and add chicken breasts, cooking each side for about 3 minutes then remove. Into same pan add lemon juice, stock, and capers. Bring to boil then re-add chicken breasts. Add any remaining butter.',
+      'image': 'piccata.jpg',
       'from': 'https://www.foodnetwork.com/recipes/giada-de-laurentiis/chicken-piccata-recipe2-1913809',
     },
 
     {'name': "Lemon Bars",
       'ingredients': '2 Cups Flour, 1 Cup Butter, 1/2 Cup Sugar, 4 Eggs, 2 Lemons (juiced)',
       'instructions': 'Blend flour, butter, and sugar into bowl until crust is formed. Press crust into pan, and blind bake crust for 15 minutes at 350°F. For filling, whisk remaining sugar and flour into bowl with eggs and lemon juice. Pour over crust and finally bake for 20 minutes. Cut into squares.',
+      'image': 'lemon.jpg',
       'from': 'https://www.allrecipes.com/recipe/10294/the-best-lemon-bars/',
     },
 
     {'name': "Hotteok",
       'ingredients': '1 Package Active Dry Yeast, 1/3 Cup Warm Water, 2 1/2 Cups Flour, 1 Tsp Salt, 1 Tbsp Sugar, 1 Tbsp Cooking Oil, 1 1/4 Cups Milk, 1/2 Cups Brown Sugar, 1 Tsp Cinnamon',
       'instructions': 'Add warm water to bowl and stir in sugar and yeast. Sift flour, sugar, and salt together and add the yeast water, oil, and milk to the flour mix. Knead until a smooth dough is formed and let rise for about an hour. Heat pan with oil. Tear a piece of the dough off and flatten the ball. Fill with brown sugar and pinch edges to form a ball. Flatten dough ball in pan with spatula and fry both sides until golden brown.',
+      'image': 'hotteok.jpg',
       'from': 'https://www.koreanbapsang.com/hotteok-sweet-korean-pancakes/',
     },
   ]; //recipe data will go here
